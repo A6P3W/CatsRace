26ca451a3567bf745daa2f5d8ded7c7f0aab3def
INUBASO\ryout <inu.game.create@gmail.com>
2026-05-29 15:37:49 +0900

    [feature]サブプロジェクトの更新とゲームロジックの改善
    BroccoliEngineのサブプロジェクトのコミットが更新され、「dirty」フラグが削除されました。
    `AGameScene01::RaceStart`メソッドでは、`SpeedUpItem`が複数回スポーンされるように変更されました。
    `APlayer`クラスのコンストラクタにスプライトコンポーネントが追加され、アニメーションリソースがロードされるようになりました。
    `APlayer::OnUpdate`メソッド内で移動とステアリングのロジックが改善され、加速度とスライダーの入力が適切に処理されるようになりました。
    また、`APlayer`クラスに新しいプライベートメンバーが追加され、`TitleScene`のコンストラクタで新しいスプライトがタイトル画面に追加されました。

--- Game/Objects/Player.h
+++ Game/Objects/Player.h
@@ -11,7 +11,7 @@ class MSpriteComponent;
 class APlayer : public APawn
 {
 public:
-	DEFINE_ACTOR_CLASS(APlayer);
+
     APlayer(FVector2D location, FRotator rotation);
 
 	void OnUpdate(float DeltaTime) override;
@@ -24,8 +24,15 @@ private:
     MMovementComponent* m_movement = nullptr;
     MSpriteComponent* m_sprite = nullptr;
     std::array<int, 5> m_walkAnimHandles{};
+private:
+    float m_accelInput = 0.0f;
     float m_slider = 0.0f;
-    float m_prevSpeed = 0.0f;
+    
+    const float MaxSpeed = 70.0f;  
+    const float MaxReverseSpeed = 20.0f; 
+    const float AccelForce = 3.5f;   
+    const float ReverseForce = 2.0f;   
+    const float MaxSteer = 5.5f;
     float m_moveAnimTime = 0.0f;
     int m_walkAnimFrame = 0;
 
--- Game/Objects/TitleScene.cpp
+++ Game/Objects/TitleScene.cpp
@@ -3,10 +3,17 @@
 #include "InputManager.h"
 #include "SceneManager.h"
 #include "Objects/GameScene01.h"
+#include "ResourceManager.h"
 #include <KeyboardDevice.h>
 #include <DxLib.h>
 ATitleScene::ATitleScene()
 {
+    int handle = ResourceManager::GetInstance().LoadResourceGraph("images/CatsRace.png");
+    auto imgSprite = std::make_unique<MSpriteComponent>(0, RenderSpace::Screen);
+    imgSprite->SubmitGraph(0.8, handle);
+    imgSprite->SetRelativeLocation({ 480.0f, 270.0f }); 
+    AddComponent(std::move(imgSprite));
+
     auto textComp = std::make_unique<MSpriteComponent>(100, RenderSpace::Screen);
     textComp->SubmitText("Press R to Start", 0xFFFF00, -1, 255);
     textComp->SetRelativeLocation({ 800.0f, 500.0f });
