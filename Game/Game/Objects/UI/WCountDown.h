f04bfd9a67cb488bf2ad2a67d23b8b989ff3473d
A6P3W <186453117+A6P3W@users.noreply.github.com>
2026-06-16 22:31:26 +0900

    [feature] UIをUISystem構造に移行

---
+++ Game/Game/Objects/UI/WCountDown.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include <WidgetBase.h>
+#include <string>
+
+class UITextComponent;
+
+class WCountDown : public AWidgetBase
+{
+public:
+	DEFINE_ACTOR_CLASS(WCountDown);
+	WCountDown();
+	void SetCountText(const std::string& text);
+
+private:
+	UITextComponent* m_Text = nullptr;
+};
