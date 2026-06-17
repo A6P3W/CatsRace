dbfc0d1d1132ebefe2f0d584d17ebf5eaebd5633
A6P3W <186453117+A6P3W@users.noreply.github.com>
2026-06-17 20:00:50 +0900

    [Update] Broccoliのコミットを7f05d74fに変更し、対応

--- Game/Game/Objects/GameScene01.cpp
+++ Game/Game/Objects/GameScene01.cpp
@@ -49,12 +49,12 @@ void AGameScene01::BeginPlay()
 
 
 	m_MainHUD = SpawnActor<WMainHUD>();
-	UIManager::GetInstance()->PushWidget(m_MainHUD);
+	UIManager::GetInstance()->AddWidget(m_MainHUD);
 
 
 	m_CountDownWidget = SpawnActor<WCountDown>();
 	m_CountDownWidget->SetCountText(std::to_string(m_CountDown));
-	UIManager::GetInstance()->PushWidget(m_CountDownWidget);
+	UIManager::GetInstance()->AddWidget(m_CountDownWidget);
 
 	GetPlayerController()->SetInputMode(EInputMode::UIOnly);
 
@@ -64,9 +64,6 @@ void AGameScene01::BeginPlay()
 void AGameScene01::RaceFinish()
 {
 	RaceRunning = false;
-	while (UIManager::GetInstance()->GetPeekWidget()) {
-		UIManager::GetInstance()->PopWidget();
-	}
 	auto gi = dynamic_cast<GI_main*>(SceneManager::GetInstance().GetGameInstance());
 	gi->ClearTime = RaceTime;
 
@@ -103,6 +100,6 @@ void AGameScene01::RaceStart()
 
 void AGameScene01::ClearCountDown()
 {
-	UIManager::GetInstance()->PopWidget();
+	UIManager::GetInstance()->RemoveWidget(m_CountDownWidget);
 	m_CountDownWidget = nullptr;
 }
