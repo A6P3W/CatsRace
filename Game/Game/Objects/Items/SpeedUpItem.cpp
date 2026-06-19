8b6bc87312bcf5bd530b5b553ed56996a57eb276
INUBASO\ryout <inu.game.create@gmail.com>
2026-06-19 14:56:51 +0900

    ドリフト機能の追加

--- Game/Game/Objects/Items/SpeedUpItem.cpp
+++ Game/Game/Objects/Items/SpeedUpItem.cpp
@@ -27,7 +27,7 @@ void SpeedUpItem::BeginOverlap(AActor* OtherActor)
 {
 	if (auto* player = dynamic_cast<APlayer*>(OtherActor)) {
 		player->GetComponents<MMovementComponent>()[0]->AddLocalForce({ 0, -25.0f });
-		player->ApplyFOVEffect(0.7f, 2.0f);
+		player->ApplyFOVEffect(0.7f, 2.0f, true);  
 		if (m_sound) m_sound->PlaySE("images/cat2d.mp3", false); // お好みのSEパスに変更
 		M_LOG("Speed Up!");
 	}
