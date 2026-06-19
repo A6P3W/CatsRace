77fc7bbaee080bc0dad988411cd7cab0f39bd4b8
A6P3W <186453117+A6P3W@users.noreply.github.com>
2026-06-19 12:07:02 +0900

    [feature] タイトルの画像サイズ変更。

--- Game/Game/Scenes/Title/TitleScene.cpp
+++ Game/Game/Scenes/Title/TitleScene.cpp
@@ -16,19 +16,22 @@ ATitleScene::ATitleScene()
     int handle = ResourceManager::GetInstance().LoadResourceGraph("images/CatsRace.png");
     auto imgSprite = std::make_unique<MSpriteComponent>(0, RenderSpace::Screen);
     imgSprite->SubmitGraph( handle);
-    imgSprite->SetRelativeLocation({ 480.0f, 270.0f }); 
+    imgSprite->SetRelativeLocation({ 960.0f, 540.0f }); 
+	imgSprite->SetWorldScale(FScale(1.5));
     AddComponent(std::move(imgSprite));
 }
 
 void ATitleScene::BeginPlay()
 {
 	AGameModeBase::BeginPlay();
-	SpawnPlayer<APawn, APlayerController>({ 0, 0 }, 0);
+
 	// Setup input and PlayerController for UI interaction
+	SpawnPlayer<APawn, APlayerController>({ 0, 0 }, 0);
 	GetPlayerController()->SetInputMode(EInputMode::UIOnly);
 
 	// Spawn and add Title HUD
 	m_TitleHUD = SpawnActor<WTitleHUD>();
 	UIManager::GetInstance()->AddWidget(m_TitleHUD);
 	UIManager::GetInstance()->SetFocusedWidget(m_TitleHUD);
+
 }
