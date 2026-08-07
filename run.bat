09458014a2e722a78ca8e42c873bf96abd16769c
A6P3W <186453117+A6P3W@users.noreply.github.com>
2026-08-07 23:48:29 +0900

    [feature] Lobby大幅改修 [skip cr] (#120)
    * [feature] Stage3Preview画像変更
    
    * [feature] PracticeSceneに画像追加
    
    * [feature] Lobby大幅改修

---
+++ run.bat
@@ -0,0 +1,33 @@
+@echo off
+setlocal
+
+set "TARGET=Debug"
+set "ARGS="
+
+if /i "%~1"=="Debug" (
+    set "TARGET=Debug"
+    shift
+) else if /i "%~1"=="Release" (
+    set "TARGET=Release"
+    shift
+)
+
+:loop
+if "%~1"=="" goto continue
+set "ARGS=%ARGS% %1"
+shift
+goto loop
+:continue
+
+set "EXE_PATH=%~dp0Publish\%TARGET%\CatsRace.exe"
+
+if exist "%EXE_PATH%" (
+    echo [run] %EXE_PATH%%ARGS%
+    start "" "%EXE_PATH%"%ARGS%
+) else (
+    echo [error] not found
+    echo path: %EXE_PATH%
+    exit /b 1
+)
+
+endlocal
