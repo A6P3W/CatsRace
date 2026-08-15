0ab97165da6ddb80de5e910a2d80c03bd0af7f9f
A6P3W <186453117+A6P3W@users.noreply.github.com>
2026-08-15 21:43:12 +0900

    [feature] ドリフト時のタイヤ痕の代わりに常時足跡を生成するように変更。 (#129)
    * [feature] ドリフト時のタイヤ痕の代わりに常時足跡を生成するように変更。
    
    * [fix] 足跡のネットワーク同期問題を修正。エンジンのコミットをe1a4f5adに更新。

--- CatsRace/Source/Entry.cpp
+++ CatsRace/Source/Entry.cpp
@@ -22,7 +22,7 @@ namespace {
 void SetupGame() {
   ImGui::SetCurrentContext(static_cast<ImGuiContext*>(Application::GetImGuiContext()));
   EOSCoreManager::GetInstance().InitializeOnlineServices();
-  NetworkManager::GetInstance().SetTransportType(ENetworkTransportType::EOSP2P);
+  NetworkManager::GetInstance().SetTransportType(ENetworkTransportType::ENet);
 
   auto& sceneManager = SceneManager::GetInstance();
   sceneManager.SetGameInstance<GI_main>();
