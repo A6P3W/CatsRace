6c8a97b34030fa1c4af56c0f1f08eef763f9104d
A6P3W <186453117+A6P3W@users.noreply.github.com>
2026-08-19 02:31:06 +0900

    [feature] マップバージョンを３に変更

--- CatsRace/Source/Core/MapData.h
+++ CatsRace/Source/Core/MapData.h
@@ -16,17 +16,17 @@ inline const std::vector<FMapInfo> AvailableMaps = {
      "Resources-EOS/Stage1/Stage1.BLevel",
      "Resources-EOS/Stage1/Stage1_Preview.png",
      "stage1",
-     1},
+     3},
     {"Stage2",
      "Resources-EOS/Stage2/Stage2.BLevel",
      "Resources-EOS/Stage2/Stage2_Preview.png",
      "stage2",
-     1},
+     3},
     {"Stage3",
      "Resources-EOS/Stage3/Stage3.BLevel",
      "Resources-EOS/Stage3/Stage3_Preview.png",
      "stage3",
-     1},
+     3},
 };
 
 inline const FMapInfo* FindMapInfo(const std::string& LevelPath) {
